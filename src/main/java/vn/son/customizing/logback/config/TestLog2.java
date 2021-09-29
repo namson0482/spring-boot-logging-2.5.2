615787b7c440315bf287198c48a0aef474462c79
namson0482@gmail.com <namson0482@gmail.com>
2021-09-29 21:40:12 +0700

    Reset all

--- src/main/java/vn/son/customizing/logback/config/TestLog2.java
+++ src/main/java/vn/son/customizing/logback/config/TestLog2.java
@@ -4,23 +4,5 @@ public class TestLog2 {
 	
 	public static void main(String[] args) {
 		System.out.println("Line 1");
-		
-		System.out.println("fix bug 1");
-		
-		System.out.println("fix bug 2");
-	}
-	
-	void addNewFunction_1() {
-		System.out.println("New function 1");
-	}
-	void addNewFunction_2() {
-		System.out.println("New function 2");
-	}
-	
-	void addNewFunction_1() {
-		System.out.println("New function 1");
-	}
-	void addNewFunction_2() {
-		System.out.println("New function 2");
 	}
 }
